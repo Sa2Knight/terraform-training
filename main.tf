fb9df8437acbcabb6492b46879148847a8f67d48
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-06-01 23:27:39 +0900

    HTTPアクセスを許可するセキュリティグループを作成

--- main.tf
+++ main.tf
@@ -46,6 +46,28 @@ resource "aws_network_interface" "my_network_interface" {
   }
 }
 
+resource "aws_security_group" "my_security_group" {
+  name = "terraform-security-group"
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    description      = "allow http"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
 resource "aws_instance" "my_instance" {
   ami           = "ami-0c3fd0f5d33134a76"
   instance_type = "t3.micro"
