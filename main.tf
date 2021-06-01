2c8a1cca255533122c97ac3d89206d4b88c2359d
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-06-01 22:53:03 +0900

    data を用いて、最新のAMIを取得するサンプル

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+data "aws_ami" "recent_amazon_linux_2" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-2.0.????????-x86_64-gp2"]
+  }
+
+  filter {
+    name = "state"
+    values = ["available"]
+  }
+}
+
 output "instance_id" {
   value = aws_instance.my_instance.id
 }
