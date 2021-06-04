d87252774811a9e49e359957de570531918c8dca
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-06-04 23:34:25 +0900

    IAMロールを作成するモジュールを作成する

--- main.tf
+++ main.tf
@@ -3,6 +3,14 @@ provider "aws" {
   version = "2.20.0"
 }
 
+data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect = "Allow"
+    actions = ["ec2:DescribeRegions"]
+    resources = ["*"]
+  }
+}
+
 resource "aws_vpc" "my_vpc" {
   cidr_block = "172.16.0.0/16"
 
@@ -18,4 +26,11 @@ resource "aws_subnet" "my_subnet" {
   tags = {
     Name = "terraform-subnet"
   }
+}
+
+module "describe_regions_for_ec2" {
+  source = "./iam_role"
+  name = "describe-regions-for-ec2"
+  identifier = "ec2.amazonaws.com"
+  policy = data.aws_iam_policy_document.allow_describe_regions.json
 }
