252526ffddaa412e5f2024ee7a442044c6c55bbb
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-06-04 23:34:04 +0900

    一旦一通りのリソースを削除

--- main.tf
+++ main.tf
@@ -3,13 +3,6 @@ provider "aws" {
   version = "2.20.0"
 }
 
-module "web_server" {
-  source = "./http_server"
-  instance_type = "t2.micro"
-  vpc_id = aws_vpc.my_vpc.id
-  subnet_id = aws_subnet.my_subnet.id
-}
-
 resource "aws_vpc" "my_vpc" {
   cidr_block = "172.16.0.0/16"
 
