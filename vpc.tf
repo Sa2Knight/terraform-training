b988ee41a5274045af00d677b7b78b83618caf49
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-06-06 00:15:02 +0900

    プライベートサブネットに、インターネットゲートウェイを設定したルートテーブルを紐付ける

--- vpc.tf
+++ vpc.tf
@@ -34,3 +34,14 @@ resource "aws_route_table" "public" {
     Name = "public"
   }
 }
+
+resource "aws_route" "public" {
+  route_table_id = aws_route_table.public.id
+  gateway_id = aws_internet_gateway.example.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
