edb00e9f00f0295d6642773398f9b950baa9221c
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-06-06 00:19:50 +0900

    プライベートサブネットとルーティング設定を作成

--- vpc.tf
+++ vpc.tf
@@ -19,6 +19,17 @@ resource "aws_subnet" "public" {
   }
 }
 
+resource "aws_subnet" "private" {
+  vpc_id = aws_vpc.example.id
+  cidr_block = "10.0.64.0/24"
+  map_public_ip_on_launch = false
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    Name = "private"
+  }
+}
+
 resource "aws_internet_gateway" "example" {
   vpc_id = aws_vpc.example.id
 
@@ -35,13 +46,27 @@ resource "aws_route_table" "public" {
   }
 }
 
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.example.id
+
+  tags = {
+    Name = "private"
+  }
+}
+
 resource "aws_route" "public" {
   route_table_id = aws_route_table.public.id
   gateway_id = aws_internet_gateway.example.id
   destination_cidr_block = "0.0.0.0/0"
 }
 
+
 resource "aws_route_table_association" "public" {
   subnet_id = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
 }
