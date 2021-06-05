bd164dbee3864457c8070d0b52e17fc3c2822657
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-06-05 23:37:12 +0900

    勝手に作ってた VPN をまるごと削除しておく

--- main.tf
+++ main.tf
@@ -2,35 +2,3 @@ provider "aws" {
   region = "ap-northeast-1"
   version = "2.20.0"
 }
-
-data "aws_iam_policy_document" "allow_describe_regions" {
-  statement {
-    effect = "Allow"
-    actions = ["ec2:DescribeRegions"]
-    resources = ["*"]
-  }
-}
-
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "172.16.0.0/16"
-
-  tags = {
-    Name = "terraform-vpc"
-  }
-}
-
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.10.0/24"
-
-  tags = {
-    Name = "terraform-subnet"
-  }
-}
-
-module "describe_regions_for_ec2" {
-  source = "./iam_role"
-  name = "describe-regions-for-ec2"
-  identifier = "ec2.amazonaws.com"
-  policy = data.aws_iam_policy_document.allow_describe_regions.json
-}
