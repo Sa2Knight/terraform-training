30b58fdd193463f143e1abc8c32e47d7518143f2
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-06-06 17:15:17 +0900

    ACMを使って証明書を作成し、DNSで検証するように

--- route53.tf
+++ route53.tf
@@ -14,6 +14,29 @@ resource "aws_route53_record" "example" {
   }
 }
 
+resource "aws_route53_record" "example_certificate" {
+  name = aws_acm_certificate.example.domain_validation_options[0].resource_record_name
+  type = aws_acm_certificate.example.domain_validation_options[0].resource_record_type
+  records = [aws_acm_certificate.example.domain_validation_options[0].resource_record_value]
+  zone_id = aws_route53_zone.example.zone_id
+  ttl = 60
+}
+
+resource "aws_acm_certificate" "example" {
+  domain_name = aws_route53_zone.example.name
+  subject_alternative_names = []
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate_validation" "example" {
+  certificate_arn = aws_acm_certificate.example.arn
+  validation_record_fqdns = [ aws_route53_record.example_certificate.fqdn]
+}
+
 output "domain_name" {
   value = aws_route53_record.example.name
 }
