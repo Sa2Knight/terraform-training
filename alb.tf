ccb8eb185ddcb9f413535a64e3c017449dfbbbc5
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-06-06 17:28:02 +0900

    HTTPからHTTPSにリダイレクトするALBリスナを追加

--- alb.tf
+++ alb.tf
@@ -57,6 +57,22 @@ resource "aws_lb_listener" "https" {
   }
 }
 
+resource "aws_lb_listener" "redirect_http_to_https" {
+  load_balancer_arn = aws_lb.example.arn
+  port = "8080"
+  protocol = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port = "443"
+      protocol = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
 output "alb_dns_name" {
   value = aws_lb.example.dns_name
 }
