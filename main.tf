d54e4bb647e23246cf844bf18bdb2993bad9c642
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-06-10 22:59:03 +0900

    セッションマネージャの下準備まで

--- main.tf
+++ main.tf
@@ -2,3 +2,42 @@ provider "aws" {
   region = "ap-northeast-1"
   version = "2.20.0"
 }
+
+resource "aws_iam_instance_profile" "ec2_for_ssm" {
+  name = "ec2-for-ssm"
+  role = module.ec2_for_ssm_role.iam_role_name
+}
+
+module "ec2_for_ssm_role" {
+  source = "./iam_role"
+  name = "ec2-for-ssm"
+  identifier = "ec2.amazonaws.com"
+  policy = data.aws_iam_policy_document.ec2_for_ssm.json
+}
+
+data "aws_iam_policy_document" "ec2_for_ssm" {
+  source_json = data.aws_iam_policy.ec2_for_ssm.policy
+
+  statement {
+    effect = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "s3:PutObject",
+      "logs:PutLogEvents",
+      "logs:CreateLogStream",
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:BatchGetImage",
+      "ecr:GetDownloadUrlForLayer",
+      "ssm:GetParameter",
+      "ssm:GetParameters",
+      "ssm:GetParametersByPath",
+      "kms:Decrypt",
+    ]
+  }
+}
+
+data "aws_iam_policy" "ec2_for_ssm" {
+  arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
