5f358f2af02d384d526bce211af6ba530275c86b
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-06-11 00:32:59 +0900

    count を使った複数リソースの一括作成

--- main.tf
+++ main.tf
@@ -2,3 +2,13 @@ provider "aws" {
   region  = "ap-northeast-1"
   version = "2.20.0"
 }
+
+resource "aws_s3_bucket" "examples" {
+  count = 5
+  bucket = "sasaki-terraform-test-${count.index}"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket ${count.index}"
+  }
+}
