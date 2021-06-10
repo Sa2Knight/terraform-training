9502758061ca2f52c60e4cd2daf674e0b5122270
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-06-11 00:07:09 +0900

    セッションマネージャー完了

--- main.tf
+++ main.tf
@@ -1,6 +1,18 @@
 provider "aws" {
-  region = "ap-northeast-1"
+  region  = "ap-northeast-1"
   version = "2.20.0"
+}
+
+resource "aws_instance" "example_for_operation" {
+  ami                  = "ami-0c3fd0f5d33134a76"
+  instance_type        = "t3.micro"
+  iam_instance_profile = aws_iam_instance_profile.ec2_for_ssm.name
+  subnet_id            = aws_subnet.private_0.id
+  user_data            = file("./user_data.sh")
+}
+
+output "operation_instance_id" {
+  value = aws_instance.example_for_operation.id
 }
 
 resource "aws_iam_instance_profile" "ec2_for_ssm" {
@@ -8,18 +20,51 @@ resource "aws_iam_instance_profile" "ec2_for_ssm" {
   role = module.ec2_for_ssm_role.iam_role_name
 }
 
+resource "aws_s3_bucket" "operation" {
+  bucket = "sasaki-operation-pragmatic-terraform"
+  lifecycle_rule {
+    enabled = true
+    expiration {
+      days = "180"
+    }
+  }
+}
+
+resource "aws_cloudwatch_log_group" "operation" {
+  name = "/operation"
+  retention_in_days = 180
+}
+
+resource "aws_ssm_document" "session_manager_run_shell" {
+  name = "SSM-SessionManagerRunShell"
+  document_type = "Session"
+  document_format = "JSON"
+
+  content = <<EOF
+  {
+    "schemaVersion": "1.0",
+    "description": "Document to hold regional settings for Session Manager",
+    "sessionType": "Standard_Stream",
+    "inputs": {
+      "s3BucketName": "${aws_s3_bucket.operation.id}",
+      "cloudWatchLogGroupName": "${aws_cloudwatch_log_group.operation.name}"
+    }
+  }
+  EOF
+}
+
 module "ec2_for_ssm_role" {
-  source = "./iam_role"
-  name = "ec2-for-ssm"
+  source     = "./iam_role"
+  name       = "ec2-for-ssm"
   identifier = "ec2.amazonaws.com"
-  policy = data.aws_iam_policy_document.ec2_for_ssm.json
+  policy     = data.aws_iam_policy_document.ec2_for_ssm.json
 }
 
 data "aws_iam_policy_document" "ec2_for_ssm" {
   source_json = data.aws_iam_policy.ec2_for_ssm.policy
 
   statement {
-    effect = "Allow"
+    effect    = "Allow"
     resources = ["*"]
 
     actions = [
--- vpc.tf
+++ vpc.tf
@@ -18,3 +18,14 @@ resource "aws_subnet" "public_0" {
     Name = "public_0"
   }
 }
+
+resource "aws_subnet" "private_0" {
+  vpc_id = aws_vpc.example.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = false
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    Name = "private_0"
+  }
+}
