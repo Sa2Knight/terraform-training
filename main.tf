3e2497a69a6787f46ca31a618591aa0bcb02bcbf
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-06-12 16:46:43 +0900

    余計なアウトプットも削除

--- main.tf
+++ main.tf
@@ -2,9 +2,3 @@ provider "aws" {
   region  = "ap-northeast-1"
   version = "2.20.0"
 }
-
-data "aws_elb_service_account" "current" { }
-
-output "account_id" {
-  value = data.aws_elb_service_account.current.id
-}
